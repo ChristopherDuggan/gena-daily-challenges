79350f518e57bd047060fffad8fa2bf6b73e7d3d
ChristopherDuggan <christopherduggan@gmail.com>
2025-05-06 09:09:06 -0400

    complete 04

---
+++ 04.js
@@ -0,0 +1,27 @@
+/*-----------------------------------------------------------------------------
+Challenge: 04-addList
+
+Difficulty: Basic
+
+Prompt:
+
+- Write a function called addList that accepts any quantity of numbers as 
+  arguments, adds them together, and returns the resulting sum.
+- Assume all parameters will be numbers.
+- If called with no arguments, return 0 (zero).
+
+Examples:
+
+addList(1) //=> 1
+addList(1,50,1.23) //=> 52.23
+addList(7,-12) //=> -5
+-----------------------------------------------------------------------------*/
+// Your solution for 04-addList here:
+
+
+const addList = (...nums) => {
+  if (nums.length === 0) return 0;
+  return nums.reduce((acc, current) => acc + current);
+}
+
+console.log(addList(7, -12));
